61703e1a88eae330ea4f119e8a1cd5c45a3875cf
fitronansori <fitronansori7@gmail.com>
2024-05-06 22:58:35 +0800

    feat: Add data constants for houses, hotels, and apartments

--- client/src/constants/data.ts
+++ client/src/constants/data.ts
@@ -3,6 +3,18 @@ import most2 from "@/assets/images/most2.jpg";
 import most3 from "@/assets/images/most3.jpg";
 import most4 from "@/assets/images/most4.jpg";
 import most5 from "@/assets/images/most5.jpg";
+import house1 from "@/assets/images/house1.jpg";
+import house2 from "@/assets/images/house2.jpg";
+import house3 from "@/assets/images/house3.jpg";
+import house4 from "@/assets/images/house4.jpg";
+import hotel1 from "@/assets/images/hotel1.jpg";
+import hotel2 from "@/assets/images/hotel2.jpg";
+import hotel3 from "@/assets/images/hotel3.jpg";
+import hotel4 from "@/assets/images/hotel4.jpg";
+import apartment1 from "@/assets/images/apartment1.jpg";
+import apartment2 from "@/assets/images/apartment2.jpg";
+import apartment3 from "@/assets/images/apartment3.jpg";
+import apartment4 from "@/assets/images/apartment4.jpg";
 
 export const mostPicked = [
   {
@@ -44,5 +56,98 @@ export const mostPicked = [
     country: "Maldives",
     price: 300,
     image: most5,
+  },
+];
+
+export const houses = [
+  {
+    id: 1,
+    title: "Tabby Town",
+    city: "Gunung Batu",
+    country: "Indonesia",
+    img: house1,
+  },
+  {
+    id: 2,
+    title: "Hill House",
+    city: "Lombok",
+    country: "Indonesia",
+    img: house2,
+  },
+  {
+    id: 3,
+    title: "Pine Tree",
+    city: "Malang Timur",
+    country: "Indonesia",
+    img: house3,
+  },
+  {
+    id: 4,
+    title: "Cottage",
+    city: "Puncak",
+    country: "Indonesia",
+    img: house4,
+  },
+];
+
+export const hotels = [
+  {
+    id: 1,
+    title: "Grand Palace",
+    city: "Jakarta",
+    country: "Indonesia",
+    img: hotel1,
+  },
+  {
+    id: 2,
+    title: "The Ritz",
+    city: "Bali",
+    country: "Indonesia",
+    img: hotel2,
+  },
+  {
+    id: 3,
+    title: "The Grand",
+    city: "Phuket",
+    country: "Thailand",
+    img: hotel3,
+  },
+  {
+    id: 4,
+    title: "The Plaza",
+    city: "Queenstown",
+    country: "New Zealand",
+    img: hotel4,
+  },
+];
+
+export const apartments = [
+  {
+    id: 1,
+    title: "The Mansion",
+    city: "Jakarta",
+    country: "Indonesia",
+    img: apartment1,
+  },
+  {
+    id: 2,
+    title: "The Oasis",
+    city: "Bali",
+    country: "Indonesia",
+    img: apartment2,
+  },
+  {
+    id: 3,
+    title: "The Grand",
+    city: "Phuket",
+    country: "Thailand",
+    img: apartment3,
+  },
+  {
+    id: 4,
+    title: "The Plaza",
+    city: "Queenstown",
+    country: "New Zealand",
+    img: apartment4,
   },
 ];
