bd87f1b2966b9d99a11093800ef9c05b01827f8d
fitronansori <fitronansori7@gmail.com>
2024-05-06 13:47:55 +0800

    feat: Add MostPicked component to Home page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/layouts/hero/Hero";
+import MostPicked from "@/components/layouts/mostpicked/MostPicked";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <MostPicked />
     </>
   );
 };
