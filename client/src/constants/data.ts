b2e04dedb5ddbd988300e82f6e02601f732de5ba
fitronansori <fitronansori7@gmail.com>
2024-05-06 13:47:25 +0800

    feat: Add mostPicked data to constants

---
+++ client/src/constants/data.ts
@@ -0,0 +1,48 @@
+import most1 from "@/assets/images/most1.jpg";
+import most2 from "@/assets/images/most2.jpg";
+import most3 from "@/assets/images/most3.jpg";
+import most4 from "@/assets/images/most4.jpg";
+import most5 from "@/assets/images/most5.jpg";
+
+export const mostPicked = [
+  {
+    id: 1,
+    title: "Shayna",
+    city: "Jakarta",
+    country: "Indonesia",
+    price: 50,
+    image: most1,
+  },
+  {
+    id: 2,
+    title: "Villa Cinta",
+    city: "Bali",
+    country: "Indonesia",
+    price: 100,
+    image: most2,
+  },
+  {
+    id: 3,
+    title: "Luxury Resort",
+    city: "Phuket",
+    country: "Thailand",
+    price: 200,
+    image: most3,
+  },
+  {
+    id: 4,
+    title: "Mountain Retreat",
+    city: "Queenstown",
+    country: "New Zealand",
+    price: 150,
+    image: most4,
+  },
+  {
+    id: 5,
+    title: "Beachfront Paradise",
+    city: "Maldives",
+    country: "Maldives",
+    price: 300,
+    image: most5,
+  },
+];
